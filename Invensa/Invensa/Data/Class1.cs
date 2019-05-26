9bc3d61349becfe028c0dbca7def5ce8e156ee21
Unknown <rujsamod@gmail.com>
2019-05-26 16:58:18 +0300

    Started making database

---
+++ Invensa/Invensa/Data/Class1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Invensa.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Invensa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //public DbSet<InventoryItem> InventoryItems { get; set; }
+        //public DbSet<ServiceItem> ServiceItems { get; set; }
+
+        //public DbSet<InventoryItemCategory> InventoryItemCategories { get; set; }
+
+        //public DbSet<Cart> Carts { get; set; }
+        //public DbSet<Invoice> Invoices { get; set; }
+
+        //public DbSet<OrderedInventoryItem> OrderedInventoryItems { get; set; }
+        //public DbSet<OrderedServiceItem> OrderedServiceItems { get; set; }
+
+        //public DbSet<MM_InventoryItem_Category> MmInventoryItemCategories { get; set; }
+
+        //public DbSet<Country> Countries { get; set; }
+
+        //public DbSet<Discount> Discounts { get; set; }
+        //public DbSet<Feedback> Feedback { get; set; }
+
+        //public DbSet<Report> Reports { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Protocol> Protocols { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Questionnaire> Questionnaires { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Solution> Solutions { get; set; }
+        public DbSet<User> Users{ get; set; }
+
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Feedback>()
+                .HasKey(k => new { k.ItemId, k.UserId });
+            modelBuilder.Entity<MM_InventoryItem_Category>()
+                .HasKey(k => new { k.InventoryItemId, k.UserId });
+            modelBuilder.Entity<OrderedInventoryItem>()
+                .HasKey(k => new { k.CartId, k.ItemId });
+            modelBuilder.Entity<OrderedServiceItem>()
+                .HasKey(k => new { k.CartId, k.ServiceId });
+            modelBuilder.Entity<Cart>()
+                .HasMany(o => o.OrderedServiceItems)
+                .WithOne(o => o.Cart);
+            modelBuilder.Entity<Cart>()
+                .HasMany(o => o.OrderedInventoryItems)
+                .WithOne(o => o.Cart);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Invensa/Invensa/Models/Reservation.cs
+++ Invensa/Invensa/Models/Reservation.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 
 /**
  * @(#) Reservation.cs
