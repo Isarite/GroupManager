bdd17b15a13bac547fc09a89c4e160651b32e1b3
VYTMIK\Vytis <vytisjons@gmail.com>
2019-05-29 00:38:59 +0300

    Companies/WACC

--- Invensa/Invensa/Models/WACCForm.cs
+++ Invensa/Invensa/Models/WACCForm.cs
@@ -7,14 +7,10 @@ namespace Invensa.Models
 {
     public class WACCForm
     {
+        
         public decimal DebtPrice { get; set; }
         public decimal Debt { get; set; }
         public decimal EquitytPrice { get; set; }
         public decimal Equity { get; set; }
-        public decimal Tax { get; set; }
-        public decimal BETA { get; set; }
-        public decimal Growth { get; set; }
-
-
     }
 }
