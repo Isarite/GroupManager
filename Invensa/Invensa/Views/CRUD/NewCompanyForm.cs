67c80957183bd7b747cb07ced85537465b65e1ae
Jurevičius Domas <domjur@ktu.lt>
2019-05-28 17:33:28 +0300

    naujausias

---
+++ Invensa/Invensa/Views/CRUD/NewCompanyForm.cs
@@ -0,0 +1,31 @@
+/**
+ * @(#) NewCompanyForm.cs
+ */
+
+namespace Invensa.Companies.Views
+{
+	public class NewCompanyForm
+	{
+		public void OpenNewCompanyForm(  )
+		{
+			
+		}
+		
+		public void SubmitForm(  )
+		{
+			
+		}
+		
+		public void DisplayErrors(  )
+		{
+			
+		}
+		
+		public void ClearInputs(  )
+		{
+			
+		}
+		
+	}
+	
+}
